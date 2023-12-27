14ba5514407f4c253040c1ce7c4545c8313e911c
Carlos Augusto <carlosmecao22@gmail.com>
2023-12-26 23:28:05 -0300

    criação de estados e implementação correta de adicionar e remover participantes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,30 +1,33 @@
 import { Alert, FlatList, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { styles } from './styles'
 import { Participant } from '../../components/Participant'
+import { useState } from 'react'
 
 export default function Home() {
 
-  const participants = ['Carlos', 'Ana', 'Jack', 'Alexandra', 'Clésio', 'Edson', 'João', 'Rafael', 'Arthur', 'Alencar']
+  const [participants, setParticipants] = useState<string[]>([])
+  const [participantName, setParticipantName] = useState('')
 
   function handleParticipantAdd() {
-    if(participants.includes('Carlos')){
+    if(participants.includes(participantName)){
+      console.log('adicionou')
       return Alert.alert('Participante existe', 'Já existe um participante na lista com este nome.')
     }
-    console.log('você adicionou um participante')
+    setParticipants([...participants,participantName])
+    setParticipantName('')
   }
 
   function handleParticipantRemove(name: string) {
-    Alert.alert('Remover', 'Deseja remover participante?', [
+    Alert.alert('Remover', `Deseja remover participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: ()=>Alert.alert('Deletado!')
+        onPress: ()=>setParticipants(preState => preState.filter(part=>part!=name))
       },
       {
         text: 'Não',
         style: 'cancel'
       }
     ])
-    console.log(`você removeu o participante ${name}`)
   }
 
   return (
@@ -33,7 +36,13 @@ export default function Home() {
       <Text style={styles.eventDate}>Terça, 12 de dezembro de 2023</Text>
 
       <View style={styles.form}>
-        <TextInput style={styles.input} placeholder='Nome do paricipante' placeholderTextColor={'#6b6b6b'} />
+        <TextInput 
+          style={styles.input} 
+          placeholder='Nome do paricipante' 
+          placeholderTextColor={'#6b6b6b'} 
+          value={participantName}
+          onChangeText={setParticipantName}
+        />
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
